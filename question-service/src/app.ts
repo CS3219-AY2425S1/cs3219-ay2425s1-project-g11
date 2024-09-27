e04f7d5c4314b6758a0e122289d5f7eff28a02ca
You Wei <thamyouwei@gmail.com>
2024-09-28 01:31:40 +0800

    Add Create, Update, Delete Question

--- question-service/src/app.ts
+++ question-service/src/app.ts
@@ -3,6 +3,9 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import questionsRouter from './routes/questionsController';
 import userQuestionsRouter from './routes/userQuestionsController';
+import insertQuestionsRouter from './routes/insertQuestionsController';
+import updateQuestionsRouter from './routes/updateQuestionsController';
+import deleteQuestionsRouter from './routes/deleteQuestionsController';
 
 dotenv.config({ path: '.env.dev' });
 
@@ -14,6 +17,9 @@ app.use(express.json());
 // different routes
 app.use('/questions', questionsRouter);
 app.use('/userquestions', userQuestionsRouter);
+app.use('/createquestion', insertQuestionsRouter);
+app.use('/updatequestion', updateQuestionsRouter);
+app.use('/deletequestion', deleteQuestionsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
