7cda484d9d886d9bf5e13ce7639e48946cc569ec
kurt <kurtyjlee@gmail.com>
2024-10-15 20:52:55 +0800

    fix logout rerouting

---
+++ peerprep-fe/src/app/collaboration/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CollaborationPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-900 p-6 pt-24 text-gray-100"></div>
+  );
+};
+
+export default CollaborationPage;
